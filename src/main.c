8bfca4d473b54a6d22d1897d963a095f0d4720b2
WaGjUb <danielvalerio@alunos.utfpr.edu.br>
2016-05-24 15:39:12 -0300

    criação main, e menu

---
+++ src/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int menu()
+{
+int digitado = -1;
+
+char digitado;
+ printf("1 - Criar arquivo\n"
+	"2 - Inserir registros\n"
+	"3 - Listar registros\n"
+	"4 - Excluir registros\n"
+	);
+scanf("%c", &digitado);
+
+	switch digitado
+	{
+		case 1:
+		//TODO chama 1
+		break;
+		case 2:
+		//TODO chama 2
+		break;
+		case 3:
+		//TODO chama 3
+		break;
+		case 4: 
+		//TODO chama 4
+		break;
+		default:
+		printf("Digita uma opção válida!");
+	}
+
+}
+
+int main()
+{
+	menu
+	return (0);
+}
